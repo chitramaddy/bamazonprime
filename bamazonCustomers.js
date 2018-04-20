991b2d1abbbd0638092aca5186ee62b08844a283
Your Name <you@example.com>
2018-04-20 12:29:33 -0400

    Questions for taking customer order

--- bamazonCustomers.js
+++ bamazonCustomers.js
@@ -21,17 +21,47 @@ connection.connect(function (err) {
 });
 
 //readproducts to display the items to the customer.
-function readProducts(){
-  connection.query("SELECT item_id, product_name, department_name, price FROM products", function(err, res){
-    console.log("We have the following on sale");
-    for (var i = 0; i < res.length; i++){
-      console.log("======================");     
-      console.log("Item Id: "+res[i].item_id+"||Department :"+res[i].department_name+"|| Product: "+res[i].product_name+"||Price: "+parseFloat(res[i].price));
-
+function readProducts() {
+  connection.query("SELECT item_id, product_name, department_name, price FROM products", function (err, res) {
+    console.log("Product list");
+    for (var i = 0; i < res.length; i++) {
+      console.log("======================");
+      console.log("Id: " + res[i].item_id + "||Department: " + res[i].department_name + "|| Product: " + res[i].product_name + "||Price($): " + parseFloat(res[i].price));
     }
+    askquestions();
   })
 }
 
+//function to take customer order
+function askquestions() {
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+
+    //Take customer input for the item they would like to buy
+    inquirer.prompt([{
+      name: 'item',
+      type: 'input',
+      message: 'Please enter the Id of the item you want to buy'
+    }]).then(function (answer) {
+      var chosenItem;
+      for (var i = 0; i < results.length; i++) {
+       if (parseInt(results[i].item_id) === parseInt(answer.item)) {
+        chosenItem = results[i];
+
+          inquirer.prompt([{
+            name: 'Qty',
+            type: 'input',
+            message: 'How many of '+chosenItem.product_name+' would you like to buy?'
+          }]).then(function (answer) {
+            console.log("You placed an order for " + answer.Qty + " " + chosenItem.product_name);
+          })
+
+        }
+      }
+    })
+
+  })
+}
 
 
 
